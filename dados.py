4b0b2615c4e04897231d7f8408997430d2153dd4
Jose E S Pimentel <40005153+jespimentel@users.noreply.github.com>
2022-01-22 10:04:17 -0300

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,8 @@
+usuario = 'xxxxx@gmail.com'
+senha = 'xxxxxxxxxxxxxxxxx'
+
+good_guys = {
+'Pimentel': {'aliases': ['José Eduardo de Souza Pimentel'], 'pesquisa': ['Piracicaba', 'Recomendação', 'Resolução', 'Aviso'],
+'email': 'email@email.br'},
+'Ivan': {'aliases': ['Ivan Carneiro'], 'pesquisa': ['Americana', 'Gaema', 'Resolução', 'Aviso'],'email': 'ivancarneiro@mpsp.mp.br'}
+}
