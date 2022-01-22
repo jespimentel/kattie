079f87c2b8c53d728a6f290ce34d78d5ef762175
Jose E S Pimentel <40005153+jespimentel@users.noreply.github.com>
2022-01-22 10:18:57 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -4,5 +4,5 @@ senha = 'xxxxxxxxxxxxxxxxx'
 good_guys = {
 'Pimentel': {'aliases': ['José Eduardo de Souza Pimentel'], 'pesquisa': ['Piracicaba', 'Recomendação', 'Resolução', 'Aviso'],
 'email': 'email1@email.br'},
-'Ivan': {'aliases': ['Ivan Carneiro'], 'pesquisa': ['Americana', 'Gaema', 'Resolução', 'Aviso'],'email': 'email2@email.br'}
+'Ivan': {'aliases': ['Ivan Carneiro Castanheiro'], 'pesquisa': ['Americana', 'Gaema', 'Resolução', 'Aviso'],'email': 'email2@email.br'}
 }
